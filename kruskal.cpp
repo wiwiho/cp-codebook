7b2b22b917337ea00fd43f5ae86b34f2d4246441
wiwiho <wiwiho1211@gmail.com>
2019-08-01 01:49:21 +0800

    Add Kruskal algorithm

---
+++ kruskal.cpp
@@ -0,0 +1,45 @@
+//Kruskal algorithm for minimum spanning tree
+
+#include <bits/stdc++.h>
+
+#define iter(a) a.begin(), a.end()
+#define lsort(a) sort(iter(a))
+#define F first
+#define S second
+#define pii pair<int, int>
+#define piii pair<int, pii>
+
+using namespace std;
+
+vector<int> p;
+int n;
+
+vector<piii> e;
+
+void initDSU(){
+    for(int i = 0; i < n; i++) p[i] = i;
+}
+
+int findDSU(int x){
+    if(p[x] == x) return x;
+    p[x] = findDSU(p[x]);
+    return p[x];
+}
+
+void unionDSU(int a, int b){
+    int pa = findDSU(a), pb = findDSU(b);
+    p[pa] = pb;
+}
+
+int kruskal(){
+    int ans = 0;
+    lsort(e);
+    initDSU();
+    for(int i = 0; i < n; i++){
+        int a = e[i].S.F, b = e[i].S.S;
+        if(findDSU(a) == findDSU(b)) continue;
+        ans += e[i].F;
+        unionDSU(a, b);
+    }
+    return ans;
+}
