3c1aaf073995c4036e5a3285486a0280fb9732d0
wiwiho <wiwiho1211@gmail.com>
2019-08-03 00:47:47 +0800

    Segment tree with lazy tag change to range add and sum query

--- segment-tree-lazy-tag.cpp
+++ segment-tree-lazy-tag.cpp
@@ -1,91 +1,72 @@
-//Segment tree for range maximum query and range modify with lazy tag
+//Segment tree with lazy tag for range add and range sum query
 
 #include <bits/stdc++.h>
 
-typedef long long ll;
-
 using namespace std;
 
-const ll MAX = 2147483647;
+typedef long long ll;
 
 #define lc(a) a * 2 + 1
 #define rc(a) a * 2 + 2
 
 struct Node{
-    int max;
-    int tag = -MAX;
-    bool isLazy(){
-        return tag != -MAX;
-    }
-    int realMax(){
-        return isLazy() ? tag : max;
+    ll sum = 0;
+    ll tag = 0;
+    int size = 1;
+    ll realSum(){
+        return sum + tag * size;
     }
 };
 
 typedef struct Node Node;
 
-vector<Node> st;
 vector<int> a;
+vector<Node> st;
 
-void build(int l, int r, int id){
+void build(int l, int r, int node){
     if(l == r){
-        st[id].max = a[l];
+        st[node].sum = a[l];
         return;
     }
     int m = (l + r) / 2;
-    build(l, m, lc(id));
-    build(m + 1, r, rc(id));
-    st[id].max = max(st[lc(id)].max, st[rc(id)].max);
+    build(l, m, lc(node));
+    build(m + 1, r, rc(node));
+    st[node].sum = st[lc(node)].sum + st[rc(node)].sum;
+    st[node].size = st[lc(node)].size + st[rc(node)].size;
 }
 
-void push(int id){
-    if(!st[id].isLazy()) return;
-    st[lc(id)].tag = st[id].tag;
-    st[rc(id)].tag = st[id].tag;
-    st[id].tag = -MAX;
+void push(int node){
+    if(st[node].tag == 0) return;
+    st[lc(node)].tag += st[node].tag;
+    st[rc(node)].tag += st[node].tag;
+    st[node].sum = st[node].realSum();
+    st[node].tag = 0;
 }
 
-/**
- * @param l 查詢左界
- * @param r 查詢右界
- * @param L 節點範圍
- * @param R 節點範圍
- * @param id 節點編號
- * @return
- */
-int query(int l, int r, int L, int R, int id){
-    if(l == L && r == R) return st[id].realMax();
-    push(id);
+ll query(int l, int r, int L, int R, int node){
+    if(l == L && r == R) return st[node].realSum();
+    push(node);
     int m = (L + R) / 2;
-    if(r <= m) return query(l, r, L, m, lc(id));
-    if(l > m) return query(l, r, m + 1, R, rc(id));
-    return max(query(l, m, L, m, lc(id)), query(m + 1, r, m + 1, R, rc(id)));
+    if(r <= m) return query(l, r, L, m, lc(node));
+    if(l > m) return query(l, r, m + 1, R, rc(node));
+    return query(l, m, L, m, lc(node)) + query(m + 1, r, m + 1, R, rc(node));
 }
 
-/**
- * @param l 修改左界
- * @param r 修改右界
- * @param v 新值
- * @param L 節點範圍
- * @param R 節點範圍
- * @param id 節點編號
- */
-void modify(int l, int r, int v, int L, int R, int id){
+void modify(int l, int r, int v, int L, int R, int node){
     if(L == R){
-        st[id].max = v;
+        st[node].sum += v;
         return;
     }
     if(l == L && r == R){
-        st[id].tag = v;
+        st[node].tag += v;
         return;
     }
-    push(id);
     int m = (L + R) / 2;
-    if(r <= m) modify(l, r, v, L, m, lc(id));
-    else if(l > m) modify(l, r, v, m + 1, R, rc(id));
+    if(r <= m) modify(l, r, v, L, m, lc(node));
+    else if(l > m) modify(l, r, v, m + 1, R, rc(node));
     else{
-        modify(l, m, v, L, m, lc(id));
-        modify(m + 1, r, v, m + 1, R, rc(id));
+        modify(l, m, v, L, m, lc(node));
+        modify(m + 1, r, v, m + 1, R, rc(node));
     }
-    st[id].max = max(st[lc(id)].realMax(), st[rc(id)].realMax());
+    st[node].sum = st[lc(node)].realSum() + st[rc(node)].realSum();
 }
