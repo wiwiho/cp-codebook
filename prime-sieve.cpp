dc38cbf81b33112751195be082d2746c25d45aa6
wiwiho <wiwiho1211@gmail.com>
2019-08-06 22:23:25 +0800

    Add prime-sieve

---
+++ prime-sieve.cpp
@@ -0,0 +1,23 @@
+//Linear sieve of Eratosthenes
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+vector<int> prime;
+vector<int> p;
+void sieve(int n){
+    prime.resize(n + 1, 1);
+    for(int i = 2; i <= n; i++){
+        if(prime[i] == 1){
+            p.push_back(i);
+            prime[i] = i;
+        }
+        for(int j : p){
+            if((ll)i * j > n || j > prime[i]) break;
+            prime[i * j] = j;
+        }
+    }
+}
