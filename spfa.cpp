2eb7edac058b5b6dca01bcf2b78171d324d8f18c
wiwiho <wiwiho1211@gmail.com>
2019-08-19 23:21:07 +0800

    Fix the code in spfa.cpp is not SPFA

--- spfa.cpp
+++ spfa.cpp
@@ -1,99 +1,49 @@
-#define NDEBUG
+//SPFA Algorithm for searching shortest path between 2 vertices
+//and check whether there is any negetive cycle in the graph or not
 
 #include <bits/stdc++.h>
 
-#define StarBurstStream ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-#define iter(a) a.begin(), a.end()
-#define riter(a) a.rbegin(), a.rend()
-#define lsort(a) sort(iter(a))
-#define gsort(a) sort(riter(a))
-#define mp(a, b) make_pair(a, b)
-#define pb(a) push_back(a)
-#define pf(a) push_front(a)
-#define pob pop_back()
-#define pof pop_front()
 #define F first
 #define S second
-#define printv(a, b) {bool pvaspace=false; \
-for(auto pva : a){ \
-    if(pvaspace) b << " "; pvaspace=true;\
-    b << pva;\
-}\
-b << "\n";}
 #define pii pair<int, int>
-#define pll pair<ll, ll>
-#define modadd(a, b) (((a % MOD) + (b % MOD)) % MOD)
-#define modtimes(a, b) (((a % MOD) * (b % MOD)) % MOD)
-
-//#define TEST
-
+ 
 typedef long long ll;
-typedef unsigned long long ull;
-
+ 
 using namespace std;
 
-const ll MOD = 1000000007;
-const ll MAX = 2147483647;
+const ll INFINITE = 2147483647;
 
-int main(){
-    StarBurstStream
+int n;
+vector<vector<pii>> g;
 
-    int n;
-    cin >> n;
+int spfa(int start, int end){
 
-    vector<pii> two;
-    vector<int> one;
+    vector<int> dis(n, INFINITE);
+    int start;
+    cin >> start;
+    dis[start] = 0;
 
-    int d = 0;
+    queue<int> q;
+    q.push(start);
+    vector<bool> inq(n);
+    inq[start] = true;
+    vector<int> cnt(n);
 
-    for(int i = 0; i < n; i++){
-        int a;
-        cin >> a;
-
-        if(a == 1){
-            one.pb(i + 1);
-        }
-        else{
-            if(two.empty()){
-                d += a;
+    while(!q.empty()){
+        int v = q.front();
+        q.pop();
+        inq[v] = false;
+        for(pii p : g[v]){
+            if(!(dis[p.F] == INFINITE || dis[v] + p.S < dis[p.F])) continue;
+            cnt[p.F]++;
+            if(cnt[p.F] >= n) return -INFINITE; //negetive cycle
+            dis[p.F] = dis[v] + p.S;
+            if(!inq[p.F]){
+                inq[p.F] = true;
+                q.push(p.F);
             }
-            else{
-                d += a - 2;
-                two.back().S--;
-                a--;
-            }
-            two.pb(mp(i + 1, a));
         }
+
     }
-
-    if(d < one.size()){
-        cout << "NO\n";
-        return 0;
-    }
-
-    cout << "YES " << (two.size() - 1 + min((int)one.size(), 2)) << "\n";
-    cout << (two.size() - 1 + one.size()) << "\n";
-
-    for(int i = 0; i < two.size() - 1; i++){
-        cout << two[i].F << " " << two[i + 1].F << "\n";
-    }
-
-    int now = 0;
-
-    if(now < one.size()){
-        cout << one[now] << " " << two.back().F << "\n";
-        two.back().S--;
-        now++;
-    }
-
-    auto it = two.begin();
-    for(; now < one.size(); now++){
-        while(it != two.end() && it->S == 0) it++;
-        assert(it != two.end());
-        cout << one[now] << " " << it->F << "\n";
-        it->S--;
-    }
-
-    return 0;
-
+    return dis[end];
 }
