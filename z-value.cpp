f03d154faf30ce6b55708def621d1b428fb757ae
wiwiho <wiwiho1211@gmail.com>
2019-10-11 23:36:41 +0800

    Add Z Algorithm

---
+++ z-value.cpp
@@ -0,0 +1,18 @@
+//Z Algorithm
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> z;
+
+void build(string s, int n){
+    z.clear();
+    z.resize(n);
+    int l = 0;
+    for(int i = 0; i < n; i++){
+        if(l + z[l] >= i) z[i] = min(z[l] + l - i, z[i - l]);
+        while(i + z[i] < n && s[z[i]] == s[i + z[i]]) z[i]++;
+        if(i + z[i] > l + z[l]) l = i;
+    }
+}
