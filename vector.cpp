5198aef3aed6eb23a5b1e870324e8cd676a3d894
wiwiho <wiwiho1211@gmail.com>
2020-01-24 22:26:16 +0800

    Update vector

--- vector.cpp
+++ vector.cpp
@@ -9,36 +9,36 @@
 using namespace std;
 
 template<typename T>
-T operator+(T a, T b){
+pair<T, T> operator+(pair<T, T> a, pair<T, T> b){
     return mp(a.F + b.F, a.S + b.S);
 }
 
 template<typename T>
-T operator-(T a, T b){
+pair<T, T> operator-(pair<T, T> a, pair<T, T> b){
     return mp(a.F - b.F, a.S - b.S);
 }
 
 template<typename T>
-T operator*(T a, T b){
+pair<T, T> operator*(pair<T, T> a, T b){
     return mp(a.F * b, a.S * b);
 }
 
 template<typename T>
-T operator/(T a, T b){
+pair<T, T> operator/(pair<T, T> a, T b){
     return mp(a.F / b, a.S / b);
 }
 
 template<typename T>
-T dot(T a, T b){
+T dot(pair<T, T> a, pair<T, T> b){
     return a.F * b.F + a.S * b.S;
 }
 
 template<typename T>
-T cross(T a, T b){
+T cross(pair<T, T> a, pair<T, T> b){
     return a.F * b.S - a.S * b.F;
 }
 
 template<typename T>
-T abs2(T a){
+T abs2(pair<T, T> a){
     return a.F * a.F + a.S * a.S;
 }
