2341757065ea6bfd16361d7d3401baaa68c27794
wiwiho <wiwiho1211@gmail.com>
2020-01-24 22:19:29 +0800

    Update BIT

--- binary-indexed-tree.cpp
+++ binary-indexed-tree.cpp
@@ -1,33 +1,28 @@
-//Binary indexed tree for getting prefix sum
-//1-indexed, so vector BIT and a should be resized to n + 1
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-vector<int> BIT;
-vector<int> a;
+template<typename T>
+struct BIT{
+    
+private:
+    vector<T> bit;
+    int lowbit(int x){
+        return x & (-x);
+    }
 
-int n;
+public:
+    explicit BIT(int sz){
+        bit.resize(sz + 1);
+    }
 
-int lowbit(int x){
-    return x & (-x);
-}
+    void modify(int x, T v){
+        for(; x < bit.size(); x += lowbit(x)) bit[x] += v;
+    }
 
-void build(){
-    for(int i = 1; i <= n; i++)
-        for(int j = i; j > i - lowbit(i); j--)
-            BIT[i] += a[j];
-}
-
-void modify(int x, int v){
-    int t = a[x];
-    a[x] = v;
-    for(; x <= n; x += lowbit(x)) BIT[x] = BIT[x] - t + v;
-}
-
-int sum(int x){
-    int ans = 0;
-    for(; x; x -= lowbit(x)) ans += BIT[x];
-    return ans;
-}
+    T get(int x){
+        T ans = T();
+        for(; x; x -= lowbit(x)) ans += bit[x];
+        return ans;
+    }
+};
