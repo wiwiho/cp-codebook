8da5535b8e9118cd71dda27cd57452a6ecc34792
wiwiho <wiwiho1211@gmail.com>
2020-02-14 02:23:31 +0800

    Update segment tree

--- segment-tree.cpp
+++ segment-tree.cpp
@@ -1,62 +1,84 @@
-//Segment Tree (for range maximum query)
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-#define lc(a) a * 2 + 1
-#define rc(a) a * 2 + 2
-
+template<typename T>
 struct Node{
-    int max;
+    T v = 0, tag = 0;
+    int sz = 1, l = -1, r = -1;
+    T rv(){
+        return v + tag * sz;
+    }
+    void addTag(T t){
+        tag += t;
+    }
 };
 
-typedef struct Node Node;
+template<typename T>
+T pullValue(T b, T c){
+    return b + c;
+}
 
-vector<Node> st;
-vector<int> a;
+template<typename T>
+void pull(Node<T> &a, Node<T> &l, Node<T> &r){
+    a.v = pullValue(l.rv(), r.rv());
+    a.sz = l.sz + r.sz;
+}
 
-void build(int l, int r, int id){
-    if(l == r){
-        st[id].max = a[l];
-        return;
+template<typename T>
+void push(Node<T> &a, Node<T> &l, Node<T> &r){
+    l.addTag(a.tag);
+    r.addTag(a.tag);
+    a.v = a.rv();
+    a.tag = 0;
+}
+
+template<typename T>
+struct SegmentTree{
+    vector<Node<T>> st;
+    int cnt = 0;
+
+    explicit SegmentTree(int sz){
+        st.resize(4 * sz);
     }
-    int m = (l + r) / 2;
-    build(l, m, lc(id));
-    build(m + 1, r, rc(id));
-    st[id].max = max(st[lc(id)].max, st[rc(id)].max);
-}
 
-/**
- * @param l 查詢左界
- * @param r 查詢右界
- * @param L 節點範圍
- * @param R 節點範圍
- * @param id 節點編號
- * @return
- */
-int query(int l, int r, int L, int R, int id){
-    if(l == L && r == R) return st[id].max;
-    int m = (L + R) / 2;
-    if(r <= m) return query(l, r, L, m, lc(id));
-    if(l > m) return query(l, r, m + 1, R, rc(id));
-    return max(query(l, m, L, m, lc(id)), query(m + 1, r, m + 1, R, rc(id)));
-}
-
-/**
- * @param i 修改位置
- * @param v 新值
- * @param L 節點範圍
- * @param R 節點範圍
- * @param id 節點編號
- */
-void modify(int i, int v, int L, int R, int id){
-    if(L == R){
-        st[id].max = v;
-        return;
+    int build(int l, int r, vector<T>& o){
+        int id = cnt++;
+        if(l == r){
+            st[id].v = o[l];
+            return id;
+        }
+        int m = (l + r) / 2;
+        st[id].l = build(l, m, o);
+        st[id].r = build(m + 1, r, o);
+        pull(st[id], st[st[id].l], st[st[id].r]);
+        return id;
     }
-    int m = (L + R) / 2;
-    if(i <= m) modify(i, v, L, m, lc(id));
-    else modify(i, v, m + 1, R, rc(id));
-    st[id].max = max(st[lc(id)].max, st[rc(id)].max);
-}
+
+    void modify(int l, int r, int v, int L, int R, int id){
+        if(l == L && r == R){
+            st[id].addTag(v);
+            return;
+        }
+        int M = (L + R) / 2;
+        if(r <= M) modify(l, r, v, L, M, st[id].l);
+        else if(l > M) modify(l, r, v, M + 1, R, st[id].r);
+        else{
+            modify(l, M, v, L, M, st[id].l);
+            modify(M + 1, r, v, M + 1, R, st[id].r);
+        }
+        pull(st[id], st[st[id].l], st[st[id].r]);
+    }
+
+    T query(int l, int r, int L, int R, int id){
+        if(l == L && r == R) return st[id].rv();
+        push(st[id], st[st[id].l], st[st[id].r]);
+        int M = (L + R) / 2;
+        if(r <= M) return query(l, r, L, M, st[id].l);
+        else if(l > M) return query(l, r, M + 1, R, st[id].r);
+        else{
+            return pullValue(query(l, M, L, M, st[id].l), query(M + 1, r, M + 1, R, st[id].r));
+        }
+    }
+
+};
