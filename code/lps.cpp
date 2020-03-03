381920a428161e14c7aadd0202af12dbd5b5f96a
wiwiho <wiwiho1211@gmail.com>
2020-03-03 13:49:56 +0800

    Add LPS

---
+++ code/lps.cpp
@@ -0,0 +1,39 @@
+#define T(x) ((x) % 2 ? s[(x) / 2] : '.')
+
+string s;
+int l;
+
+int ex(int l, int r){
+    int i = 0;
+    while(l - i >= 0 && r + i < l && T(l - i) == T(r + i)) i++;
+    return i;
+}
+
+int lps(string ss){
+    s = ss;
+    l = 2 * s.size() + 1;
+
+    int mx = 0;
+    int center = 0;
+    vector<int> r(l);
+    int ans = 1;
+    r[0] = 1;
+    for(int i = 1; i < l; i++){
+        int ii = center - (i - center);
+        int len = mx - i + 1;
+        if(i > mx){
+            r[i] = ex(i, i);
+            center = i;
+            mx = i + r[i] - 1;
+        }
+        else if(r[ii] == len){
+            r[i] = len + ex(i - len, i + len);
+            center = i;
+            mx = i + r[i] - 1;
+        }
+        else r[i] = min(r[ii], len);
+        ans = max(ans, r[i]);
+    }
+
+    return ans - 1;
+}
