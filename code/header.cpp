7e83f27f3e0e5200c574dd97ff3a6b4505c1ecfc
wiwiho <wiwiho1211@gmail.com>
2020-06-03 12:04:39 +0800

    Update header
    - Rearrange.
    - Use using instead of define to declare some type aliases.
    - Add a alias of long double.
    - Update implementation of iceil.

--- code/header.cpp
+++ code/header.cpp
@@ -8,34 +8,38 @@
 #define riter(a) a.rbegin(), a.rend()
 #define lsort(a) sort(iter(a))
 #define gsort(a) sort(riter(a))
-#define mp(a, b) make_pair(a, b)
 #define pb(a) push_back(a)
 #define eb(a) emplace_back(a)
 #define pf(a) push_front(a)
 #define pob pop_back()
 #define pof pop_front()
+#define mp(a, b) make_pair(a, b)
 #define F first
 #define S second
+#define mt make_tuple
+#define gt(t, i) get<i>(t)
+#define iceil(a, b) ((a + b - 1) / b)
+#define tomax(a, b) (a = max(a, b))
 #define printv(a, b) {bool pvaspace=false; \
 for(auto pva : a){ \
     if(pvaspace) b << " "; pvaspace=true;\
     b << pva;\
 }\
 b << "\n";}
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define tiii tuple<int, int, int>
-#define mt make_tuple
-#define gt(t, i) get<i>(t)
-#define iceil(a, b) ((a) / (b) + !!((a) % (b)))
 
 //#define TEST
 
-typedef long long ll;
-typedef unsigned long long ull;
-
 using namespace std;
 using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<ld, ld>;
+using tiii = tuple<int, int, int>;
 
 const ll MOD = 1000000007;
 const ll MAX = 2147483647;
@@ -44,7 +48,6 @@ template<typename A, typename B>
 ostream& operator<<(ostream& o, pair<A, B> p){
     return o << '(' << p.F << ',' << p.S << ')';
 }
-
 
 int main(){
     StarBurstStream
