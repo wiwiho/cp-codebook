877a29fca4fcaf8b95258fb1095d38602a6cb959
wiwiho <wiwiho1211@gmail.com>
2020-06-08 13:57:15 +0800

    Fix z-value

--- code/z-value.cpp
+++ code/z-value.cpp
@@ -4,7 +4,7 @@ void build(string s, int n){
     z.clear();
     z.resize(n);
     int l = 0;
-    for(int i = 0; i < n; i++){
+    for(int i = 1; i < n; i++){
         if(l + z[l] >= i) z[i] = min(z[l] + l - i, z[i - l]);
         while(i + z[i] < n && s[z[i]] == s[i + z[i]]) z[i]++;
         if(i + z[i] > l + z[l]) l = i;
