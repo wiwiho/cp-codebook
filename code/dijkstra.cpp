9c000a5a166b9cc1378546c4182866f97acd3ce1
wiwiho <wiwiho1211@gmail.com>
2021-08-16 20:02:19 +0800

    Update Dijkstra

--- code/dijkstra.cpp
+++ code/dijkstra.cpp
@@ -1,28 +1,23 @@
-//The first element in pair should be edge weight, and the second should be vertex
-vector<vector<pii>> g;
+// pii = (vertex, weight)
+vector<vector<pll>> g;
 int n;
+const ll INF = 1LL << 60;
 
-int dijkstra(int start, int end){
-    priority_queue<pii, vector<pii>, greater<pii>> q;
-    for(pii p : g[start]){
-        q.push(p);
-    }
-    q.push(mp(0, start));
-    vector<int> dis(n, -1);
+ll dijkstra(int start, int end){
+    // pii = (dis, vertex)
+    priority_queue<pll, vector<pll>, greater<>> pq;
+    pq.push(mp(0, start));
+    vector<ll> dis(n, INF);
     dis[start] = 0;
-    vector<int> visit(n);
-    while(q.size()){
-        int v = q.top().S;
-        int d = q.top().F;
-        if(v == end) break;
-        q.pop();
-        if(visit[v]) continue;
-        visit[v] = true;
+    while(!pq.empty()){
+        int v = pq.top().S;
+        ll d = pq.top().F;
+        pq.pop();
+        if(d != dis[v]) continue;
         for(pii p : g[v]){
-            if(visit[p.S]) continue;
-            if(dis[p.S] == -1 || d + p.F < dis[p.S]){
-                dis[p.S] = d + p.F;
-                q.push(mp(dis[p.S], p.S));
+            if(d + p.S < dis[p.F]){
+                dis[p.F] = d + p.S;
+                pq.push(mp(d + p.S, p.F));
             }
         }
     }
