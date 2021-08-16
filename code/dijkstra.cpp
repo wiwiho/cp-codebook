4187afe61cf6554977aa7f75fc884c54a037081e
wiwiho <wiwiho1211@gmail.com>
2021-08-16 20:04:23 +0800

    Fix incorrect type

--- code/dijkstra.cpp
+++ code/dijkstra.cpp
@@ -1,10 +1,10 @@
-// pii = (vertex, weight)
+// pll = (vertex, weight)
 vector<vector<pll>> g;
 int n;
 const ll INF = 1LL << 60;
 
 ll dijkstra(int start, int end){
-    // pii = (dis, vertex)
+    // pll = (dis, vertex)
     priority_queue<pll, vector<pll>, greater<>> pq;
     pq.push(mp(0, start));
     vector<ll> dis(n, INF);
@@ -14,7 +14,7 @@ ll dijkstra(int start, int end){
         ll d = pq.top().F;
         pq.pop();
         if(d != dis[v]) continue;
-        for(pii p : g[v]){
+        for(pll p : g[v]){
             if(d + p.S < dis[p.F]){
                 dis[p.F] = d + p.S;
                 pq.push(mp(d + p.S, p.F));
