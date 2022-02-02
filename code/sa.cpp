9ac05921602e22e6d07a806520dffd1c52079235
wiwiho <wiwiho1211@gmail.com>
2022-02-03 01:12:20 +0800

    Update sa

--- code/sa.cpp
+++ code/sa.cpp
@@ -1,47 +1,45 @@
 #include <bits/stdc++.h>
 
 #define eb(a) emplace_back(a)
+#define iter(a) a.begin(), a.end()
+#define id(a) ((a) - 'a')
 
 using namespace std;
 
-vector<int> sa(string s){
-    s += '$';
-    int n = s.size();
-    int t = __lg(n) + 1;
+string s;
+vector<int> rk;
+vector<int> sa;
+int n;
 
-    vector<vector<int>> rk(t + 1, vector<int>(n)), b;
+void build_sa(){
+    sa.resize(n);
+    rk.resize(n);
+    vector<int> t1(n), t2(n);
+    vector<int> cnt(max(n, 27));
 
-    vector<vector<int>> c1(27);
-    for(int i = 0; i < n; i++) c1[s[i] == '$' ? 0 : s[i] - 'a' + 1].eb(i);;
-    for(int i = 0; i < 27; i++){
-        if(!c1[i].empty()) b.eb(c1[i]);
-    }
-    b.resize(n);
-    for(int i = 0; i < n; i++){
-        for(int k : b[i]) rk[0][k] = i;
-    }
+    for(int i = 0; i < n; i++) cnt[id(s[i])]++, t1[i] = id(s[i]);
+    for(int i = 1; i < 27; i++) cnt[i] += cnt[i - 1];
+    for(int i = n - 1; i >= 0; i--) sa[--cnt[id(s[i])]] = i;
+    
+    for(int k = 1; k < n; k *= 2){
+        fill(iter(cnt), 0);
+        for(int i = 0; i < n; i++) cnt[t1[i]]++;
+        for(int i = 1; i < cnt.size(); i++) cnt[i] += cnt[i - 1];
+        int p = 0;
+        for(int i = n - k; i < n; i++) t2[p++] = i;
+        for(int i = 0; i < n; i++) if(sa[i] >= k) t2[p++] = sa[i] - k;
+        for(int i = n - 1; i >= 0; i--) sa[--cnt[t1[t2[i]]]] = t2[i];
+        t2[sa[0]] = p = 0;
 
-    for(int i = 1; i <= t; i++){
-        vector<vector<int>> tb(n);
-        for(int j = 0; j < n; j++){
-            for(int k : b[j]){
-                int tmp = ((k - (1 << (i - 1))) % n + n) % n;
-                int now = rk[i - 1][tmp];
-                tb[now].eb(tmp);
-            }
+        for(int i = 1; i < n; i++){
+            int a = sa[i], b = sa[i - 1];
+            if(t1[a] == t1[b] && a + k < n && b + k < n && t1[a + k] == t1[b + k]);
+            else p++;
+            t2[sa[i]] = p;
         }
-        b = tb;
-        int cnt = -1;
-        for(int j = 0; j < n; j++){
-            int lst = -1;
-            for(int k : b[j]){
-                int now = rk[i - 1][(k + (1 << (i - 1))) % n];
-                if(now != lst) cnt++;
-                rk[i][k] = cnt;
-                lst = now;
-            }
-        }
+        if(n == p + 1) break;
+        t1.swap(t2);
     }
 
-    return rk[t];
+    for(int i = 0; i < n; i++) rk[sa[i]] = i;
 }
