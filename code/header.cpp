84b9ee449b61396e3ff425071dfd1042447e9457
wiwiho <wiwiho1211@gmail.com>
2024-02-09 01:27:57 +0800

    Update header

--- code/header.cpp
+++ code/header.cpp
@@ -1,51 +1,40 @@
 #include <bits/stdc++.h>
-#include <bits/extc++.h>
-
-#define StarBurstStream ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-#define iter(a) a.begin(), a.end()
-#define riter(a) a.rbegin(), a.rend()
-#define lsort(a) sort(iter(a))
-#define gsort(a) sort(riter(a))
-#define pb(a) push_back(a)
-#define eb(a) emplace_back(a)
-#define pf(a) push_front(a)
-#define ef(a) emplace_front(a)
-#define pob pop_back()
-#define pof pop_front()
-#define mp(a, b) make_pair(a, b)
-#define F first
-#define S second
-#define mt make_tuple
-#define gt(t, i) get<i>(t)
-#define tomax(a, b) ((a) = max((a), (b)))
-#define tomin(a, b) ((a) = min((a), (b)))
-#define topos(a) ((a) = (((a) % MOD + MOD) % MOD))
-#define uni(a) a.resize(unique(iter(a)) - a.begin())
-#define printv(a, b) {bool pvaspace=false; \
-for(auto pva : a){ \
-    if(pvaspace) b << " "; pvaspace=true;\
-    b << pva;\
-}\
-b << "\n";}
 
 using namespace std;
-using namespace __gnu_pbds;
+
+#define StarBurstStream ios_base::sync_with_stdio(false); cin.tie(0);
+#define iter(a) a.begin(), a.end()
+#define pb emplace_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define topos(a) ((a) = (((a) % MOD + MOD) % MOD))
+#define uni(a) a.resize(unique(iter(a)) - a.begin())
+#define SZ(a) int(a.size())
+#ifdef LOCAL
+void debug(){cerr << "\n";}
+template<class T, class ... U> void debug(T a, U ... b){cerr << a << " ", debug(b...);}
+template<class T> void pary(T l, T r) {
+	while (l != r) cerr << *l << " ", l++;
+	cerr << "\n";
+}
+#else
+#define debug(...) void()
+#define pary(...) void()
+#endif
 
 typedef long long ll;
-typedef unsigned long long ull;
 typedef long double ld;
 
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
-using pdd = pair<ld, ld>;
-using tiii = tuple<int, int, int>;
 
 const ll MOD = 1000000007;
 const ll MAX = 2147483647;
 
 template<typename A, typename B>
 ostream& operator<<(ostream& o, pair<A, B> p){
-    return o << '(' << p.F << ',' << p.S << ')';
+    return o << '(' << p.ff << ',' << p.ss << ')';
 }
 
 ll ifloor(ll a, ll b){
@@ -61,7 +50,7 @@ ll iceil(ll a, ll b){
 }
 
 int main(){
-    StarBurstStream
+    StarBurstStream;
 
     return 0;
 }
